7519c23923daefd59b60a815724dc8f36731ab88
Eduard Schildner <eduard.schildner@gmail.com>
2021-03-12 23:10:44 +0100

    Remove commented lines in main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,9 @@
 provider "azurerm" {
-  #tenant_id       = var.tenant_id
-  #subscription_id = var.subscription_id
-  #client_id       = var.client_id
-  #client_secret   = var.client_secret
   features {}
 }
 terraform {
   backend "azurerm" {
+    resource_group_name  = "remote-backend-rg"
     storage_account_name = "tstate19841"
     container_name       = "tstate"
     key                  = "terraform-cloud-developer-project1.tfstate"
