da5841ae5c8ec3b0a9ed40adba1ed67d8649ae65
Eduard Schildner <eduard.schildner@gmail.com>
2021-03-13 22:02:15 +0100

    Configure specific tenant access

--- config.py
+++ config.py
@@ -14,7 +14,17 @@ class Config(object):
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'udacityadmin'
     SQL_PASSWORD = os.environ.get('SQL_PASSWORD')
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' 
+                                + SQL_USER_NAME 
+                                + '@' 
+                                + SQL_SERVER 
+                                + ':' 
+                                + SQL_PASSWORD 
+                                + '@' 
+                                + SQL_SERVER 
+                                + ':1433/' 
+                                + SQL_DATABASE  
+                                + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     ### Info for MS Authentication ###
@@ -25,10 +35,11 @@ class Config(object):
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
     CLIENT_SECRET = os.getenv("CLIENT_SECRET")
     if not CLIENT_SECRET:
-        raise ValueError("Need to define CLIENT_SECRET environment variable")
+        raise ValueError("Need to define CLIENT_SECRET environment variable"
 
-    AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
-    # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
+    #AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
+    #AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
+    AUTHORITY = "https://login.microsoftonline.com/cloud-developer-nanodegree"
 
     CLIENT_ID = "899dfccc-36ef-412a-b524-9c4b0e57e2fb"
 
