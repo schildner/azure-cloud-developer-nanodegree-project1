4d03417e05c471f0a07b067693ad7b9cbe13a440
Eduard Schildner <eduard.schildner@gmail.com>
2021-03-13 22:08:06 +0100

    Fix indentation

--- config.py
+++ config.py
@@ -14,17 +14,7 @@ class Config(object):
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'udacityadmin'
     SQL_PASSWORD = os.environ.get('SQL_PASSWORD')
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' 
-                                + SQL_USER_NAME 
-                                + '@' 
-                                + SQL_SERVER 
-                                + ':' 
-                                + SQL_PASSWORD 
-                                + '@' 
-                                + SQL_SERVER 
-                                + ':1433/' 
-                                + SQL_DATABASE  
-                                + '?driver=ODBC+Driver+17+for+SQL+Server'
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     ### Info for MS Authentication ###
